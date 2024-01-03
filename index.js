b4a1b506135a381ed1b2fa50e3a1a7ddbb766e7d
HARISH <79706321+HARISHCHANDRA214@users.noreply.github.com>
2024-01-03 18:32:16 +0530

    html, css with some remaining js only code for % operator is not completed

---
+++ index.js
@@ -0,0 +1,112 @@
+// let a="9"
+// console.log(typeof(parseFloat(a))==="number")
+let lastKeyPressed="";
+let firstNumber="";
+let secondNumber="";
+let isFirstNumber=true;
+let isFirstOperand=true;
+let operator="";
+let currentDisplay="";
+let currentNumber="firstNumber";
+let isDecimalNumber=true;
+function clearDisplay(){
+    firstNumber="";
+    secondNumber="";
+    isFirstNumber=true;
+    isFirstOperand=true;
+    isDecimalNumber=true;
+    document.getElementById("display").innerHTML=0;
+}
+function numberPressed(e){
+    if(isFirstNumber){
+        firstNumber+=e.value;
+        document.getElementById("display").innerHTML=firstNumber;
+        currentNumber="firstNumber";
+    }else{
+        currentNumber="secondNumber";
+        secondNumber+=e.value;
+        document.getElementById("display").innerHTML=secondNumber;
+    }
+    lastKeyPressed="number";
+}
+function decimalPressed(){
+    if(isDecimalNumber){
+        isDecimalNumber=false;
+        if(isFirstNumber){
+            firstNumber+=".";
+            document.getElementById("display").innerHTML=firstNumber;
+        }else{
+            secondNumber+=".";
+            document.getElementById("display").innerHTML=secondNumber;
+        }
+    }
+}
+function operation(operator){
+    switch(operator){
+        case "-": 
+            firstNumber=parseFloat(firstNumber)-parseFloat(secondNumber);
+            secondNumber="";
+            document.getElementById("display").innerHTML=firstNumber;
+            break;
+        case "+":
+            firstNumber=parseFloat(firstNumber)+parseFloat(secondNumber);
+            secondNumber="";
+            document.getElementById("display").innerHTML=firstNumber;
+            break;
+        case "*":
+            firstNumber=parseFloat(firstNumber)*parseFloat(secondNumber);
+            secondNumber="";
+            document.getElementById("display").innerHTML=firstNumber;
+            break;
+        case "/":
+            firstNumber=parseFloat(firstNumber)/parseFloat(secondNumber);
+            secondNumber="";
+            document.getElementById("display").innerHTML=firstNumber;
+            break;
+        case "%":
+            firstNumber=parseFloat(firstNumber)%parseFloat(secondNumber);
+            secondNumber="";
+            document.getElementById("display").innerHTML=firstNumber;
+            break;
+
+    }
+}
+function operatorPressed(e){
+    if(e.value=="+/-"){
+        if(firstNumber==""){
+            console.log("Entered");
+            firstNumber=""+(parseFloat(currentDisplay)*-1);
+            document.getElementById("display").innerHTML=firstNumber;
+            return;
+        }
+        if(currentNumber=="firstNumber"){
+            firstNumber=""+(parseFloat(firstNumber)*-1);
+            document.getElementById("display").innerHTML=firstNumber;
+        }else{
+            secondNumber=""+(parseFloat(secondNumber)*-1);
+            document.getElementById("display").innerHTML=secondNumber;
+        }
+        return;
+    }else if(lastKeyPressed=="operator"){
+        secondNumber=firstNumber;
+    }else if(isFirstOperand){
+        isFirstNumber=false;
+        isFirstOperand=false;
+        operator=e.value;
+    }else{
+        operation(operator);
+        operator=e.value;
+    }
+    isDecimalNumber=true;
+    lastKeyPressed="operator";
+}
+function equals(){
+    lastKeyPressed="equals";
+    operation(operator);
+    currentDisplay=firstNumber;
+    firstNumber="";
+    secondNumber="";
+    isFirstNumber=true;
+    isFirstOperand=true;
+    isDecimalNumber=true;
+}
